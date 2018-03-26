be16a8b09ddcc7e8013f7bbebe32033f8dc98ff3
0driveshaft0 <sebastien.hutinet@gmail.com>
2018-03-26 14:59:10 +0200

    librairies commit 5

---
+++ R-Portable/App/R-Portable/library/mzR/unitTests/test_chromatograms.R
@@ -0,0 +1,23 @@
+test_chromatograms1 <- function() {
+    f <- proteomics(full.names = TRUE, pattern = "MRM")
+    x <- openMSfile(f, backend = "pwiz")
+    checkIdentical(nChrom(x), 138L)
+    checkIdentical(tic(x), chromatogram(x, 1L))
+    checkIdentical(chromatogram(x), chromatograms(x))
+    checkIdentical(nrow(chromatogram(x, 1L)), 85799L)
+    checkIdentical(nrow(chromatogram(x, 110L)), 591L)
+    checkIdentical(nrow(chromatogram(x, 111L)), 1004L)
+    checkIdentical(nrow(chromatogram(x, 112L)), 1004L)
+    checkIdentical(nrow(chromatogram(x, 136L)), 527L)
+    checkIdentical(nrow(chromatogram(x, 137L)), 567L)
+    checkIdentical(nrow(chromatogram(x, 138L)), 567L)
+}
+
+test_chromatograms2 <- function() {
+    f <- proteomics(full.names = TRUE,
+                    pattern = "TMT_Erwinia_1uLSike_Top10HCD_isol2_45stepped_60min_01.mzML.gz")
+    x <- openMSfile(f, backend = "pwiz")
+    checkIdentical(nChrom(x), 1L)   
+    checkIdentical(tic(x), chromatogram(x, 1L))
+    checkIdentical(nrow(tic(x)), 7534L)
+}
