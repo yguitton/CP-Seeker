2fdeef13ab25f11b83389ea55a56c555ac3dc490
shutinet <sebastien.hutinet@gmail.com>
2021-04-09 11:44:06 +0200

    add datasets from enviPat

--- utils/app.R
+++ utils/app.R
@@ -1,3 +1,15 @@
+data("isotopes", package = "enviPat")
+data("adducts", package = "enviPat")
+data("resolution_list", package = "enviPat")
+
+# order isotopes to have first carbons, then hydrogens, then elements in alphabetical order
+elts_CH <- unlist(lapply(c("C", "[12]C", "[13]C", "H", "D", "[1]H", "[2]H"), function(elt)
+	which(isotopes$element == elt)))
+isotopes_CH <- isotopes[elts_CH, ]
+isotopes_not_CH <- isotopes[-elts_CH, ]
+isotopes <- rbind(isotopes_CH, isotopes_not_CH[order(
+	isotopes_not_CH$element), ])
+
 options(warn = -1) # deactivate warnings cause of plotly (multiplication)
 options(stringsAsFactors = FALSE)
 options(browser = chromium)
