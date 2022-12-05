67b92296fbf65fbc90bd305acee85ddd92c35433
root <root@O008.laberca.fr>
2022-12-05 08:55:22 +0100

    change +/- into + on - symbol

--- server/export_function.R
+++ server/export_function.R
@@ -118,16 +118,16 @@ export_PCA <- function(user, chem_type, adducts, project_informations, pbValue, 
         openxlsx::writeData(wb, 2, "Out of m/z range, or number of halogen higher than number of carbon plus 3, or not processed", startRow = 27, startCol = 2)
         openxlsx::writeData(wb, 2, "Stadding (at least the 2 most intense isotopomer groups)", startRow = 28, startCol = 2)
         openxlsx::writeData(wb, 2, "Score threshold", startRow = 29, startCol = 2)
-        openxlsx::writeData(wb, 2, "Deviation tolerance (+/- mDa)", startRow = 30, startCol = 2)
+        openxlsx::writeData(wb, 2, "Deviation tolerance (\u00b1 mDa)", startRow = 30, startCol = 2)
         addStyle(wb, 2, boldStyle, rows = c(4,11,15,21,26), cols = 2)
         setColWidths(wb, 2, cols = 2, widths = 25)
 
         # Mass tolerance
         decParams <- deconvolution_params()[which(chem %in% deconvolution_params()$chemical_type),]
         if(decParams$ppm > 0){
-          openxlsx::writeData(wb, 2, paste0(decParams$ppm," ppm"), startCol = 3, startRow = 5)
+          openxlsx::writeData(wb, 2, paste0("\u00b1", decParams$ppm," ppm"), startCol = 3, startRow = 5)
         }else if(decParams$mda > 0){
-          openxlsx::writeData(wb, 2, paste0(decParams$mda," mDa"), startCol = 3, startRow = 5)
+          openxlsx::writeData(wb, 2, paste0("\u00b1", decParams$mda," mDa"), startCol = 3, startRow = 5)
         }
         if(decParams$instrument == "Orbitrap"){
           openxlsx::writeData(wb, 2, paste0(decParams$instrument," ",decParams$resolution/1000,"k@",decParams$resolution_mz), startCol = 3, startRow = 6)
@@ -572,7 +572,7 @@ export_PCO <- function(user, chem_type, adducts, project_informations, pbValue, 
       openxlsx::writeData(wb, 2, "Out of m/z range, or number of halogen higher than number of carbon plus 3, or not processed", startRow = 27, startCol = 2)
       openxlsx::writeData(wb, 2, "Stadding (at least the 2 most intense isotopomer groups)", startRow = 28, startCol = 2)
       openxlsx::writeData(wb, 2, "Score threshold", startRow = 29, startCol = 2)
-      openxlsx::writeData(wb, 2, "Deviation tolerance (+/- mDa)", startRow = 30, startCol = 2)
+      openxlsx::writeData(wb, 2, "Deviation tolerance (\u00b1 mDa)", startRow = 30, startCol = 2)
       addStyle(wb, 2, boldStyle, rows = c(4,11,15,21,26), cols = 2)
       setColWidths(wb, 2, cols = 2, widths = 25)
 
@@ -580,9 +580,9 @@ export_PCO <- function(user, chem_type, adducts, project_informations, pbValue, 
       decParams <- deconvolution_params()[which(deconvolution_params()$chemical_type %in% chem_type),]
       decParams <- decParams[which(decParams$adduct == adduct),]
       if(unique(decParams$ppm) > 0){
-        openxlsx::writeData(wb, 2, paste0(decParams$ppm," ppm"), startCol = 3, startRow = 5)
+        openxlsx::writeData(wb, 2, paste0("\u00b1", decParams$ppm," ppm"), startCol = 3, startRow = 5)
       }else if(decParams$mda > 0){
-        openxlsx::writeData(wb, 2, paste0(decParams$mda," mDa"), startCol = 3, startRow = 5)
+        openxlsx::writeData(wb, 2, paste0("\u00b1", decParams$mda," mDa"), startCol = 3, startRow = 5)
       }
       if(decParams$instrument == "Orbitrap"){
         openxlsx::writeData(wb, 2, paste0(decParams$instrument," ",decParams$resolution/1000,"k@",decParams$resolution_mz), startCol = 3, startRow = 6)
@@ -1195,7 +1195,7 @@ export_PXA <- function(user, chem_type, adducts, project_informations, pbValue, 
       openxlsx::writeData(wb, 2, "Out of m/z range, or number of halogen higher than number of carbon plus 3, or not processed", startRow = 27, startCol = 2)
       openxlsx::writeData(wb, 2, "Stadding (at least the 2 most intense isotopomer groups)", startRow = 28, startCol = 2)
       openxlsx::writeData(wb, 2, "Score threshold", startRow = 29, startCol = 2)
-      openxlsx::writeData(wb, 2, "Deviation tolerance (+/- mDa)", startRow = 30, startCol = 2)
+      openxlsx::writeData(wb, 2, "Deviation tolerance (\u00b1 mDa)", startRow = 30, startCol = 2)
       addStyle(wb, 2, boldStyle, rows = c(4,11,15,21,26), cols = 2)
       setColWidths(wb, 2, cols = 2, widths = 25)
 
@@ -1203,9 +1203,9 @@ export_PXA <- function(user, chem_type, adducts, project_informations, pbValue, 
       decParams <- deconvolution_params()[which(deconvolution_params()$chemical_type %in% chem_type),]
       decParams <- decParams[which(decParams$adduct == adduct),]
       if(decParams$ppm > 0){
-        openxlsx::writeData(wb, 2, paste0(unique(decParams$ppm)," ppm"), startCol = 3, startRow = 5)
+        openxlsx::writeData(wb, 2, paste0("\u00b1", unique(decParams$ppm)," ppm"), startCol = 3, startRow = 5)
       }else if(decParams$mda > 0){
-        openxlsx::writeData(wb, 2, paste0(unique(decParams$mda)," mDa"), startCol = 3, startRow = 5)
+        openxlsx::writeData(wb, 2, paste0("\u00b1", unique(decParams$mda)," mDa"), startCol = 3, startRow = 5)
       }
       if(decParams$instrument == "Orbitrap"){
         openxlsx::writeData(wb, 2, paste0(unique(decParams$instrument)," ",unique(decParams$resolution)/1000,"k@",unique(decParams$resolution_mz)), startCol = 3, startRow = 6)
