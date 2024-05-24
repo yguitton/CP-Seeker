917ce3106734895080320a3056e7104e12ae9d61
Damien <damien.rat44@gmail.com>
2024-05-24 11:50:51 +0200

    [Quanti] - Homologue selection by type (SCCP, MCCP, LCCP)

--- server/quantification.R
+++ server/quantification.R
@@ -1,3 +1,15 @@
+#####################################
+############ Sample List ############
+#####################################
+
+#' Le but ici est de laisser l'utilisateur rentrer les samples types
+#' 3 types de sample type : CAL, BLANK, SAMPLE
+#' Si le Type est égal à CAL alors faire rentrer les informations.
+#' Les infos : subclassname SCCP, MCCP, LCCP || chlorination_degree || concentration
+#' 
+#' Pour l'instant l'utilisateur peut remplir ce qu'il veut dans les colonnes qu'il veut.
+#' Par la suite ajouter des restrictions.
+
 # Créer un objet réactif pour stocker les données
 data <- reactive({
   params <- list(
@@ -60,4 +72,102 @@ observeEvent(input$quanti_table_cell_edit, {
     # Mettre à jour l'objet réactif data
     assign("data", reactive({ data_df }), envir = .GlobalEnv)
   }
+})
+
+##########################################
+############ Homologue Domain ############
+##########################################
+
+#' Le but est de retourner une matrice vide
+#' L'utilisateur va selectionner les groupes d'homologue dans la matrice
+#' La matrice va avoir C6-C36 première colonne et Cl3-Cl30 en première ligne
+
+# Les groupes d'homologues selectionnés vont être stockés
+
+# Créer une matrice vide avec les dimensions désirées
+
+# Créer les matrices avec les noms de colonnes et de lignes définis
+sccp_matrix_initial <- matrix(ncol = 31, nrow = 28)
+colnames(sccp_matrix_initial) <- paste0("C", 6:36)
+rownames(sccp_matrix_initial) <- paste0("Cl", 3:30)
+
+mccp_matrix_initial <- matrix(ncol = 31, nrow = 28)
+colnames(mccp_matrix_initial) <- paste0("C", 6:36)
+rownames(mccp_matrix_initial) <- paste0("Cl", 3:30)
+
+lccp_matrix_initial <- matrix(ncol = 31, nrow = 28)
+colnames(lccp_matrix_initial) <- paste0("C", 6:36)
+rownames(lccp_matrix_initial) <- paste0("Cl", 3:30)
+
+# Réactifs pour stocker les matrices vides pour chaque type
+sccp_matrix <- reactiveVal(as.data.frame(sccp_matrix_initial))
+mccp_matrix <- reactiveVal(as.data.frame(mccp_matrix_initial))
+lccp_matrix <- reactiveVal(as.data.frame(lccp_matrix_initial))
+
+# Réactifs pour stocker les sélections pour chaque type
+selected_cells_sccp <- reactiveVal(data.frame(row = integer(0), col = integer(0)))
+selected_cells_mccp <- reactiveVal(data.frame(row = integer(0), col = integer(0)))
+selected_cells_lccp <- reactiveVal(data.frame(row = integer(0), col = integer(0)))
+
+# Render DataTable en fonction du type sélectionné
+output$quanti_matrix_homologue <- DT::renderDataTable({
+  matrix_to_display <- switch(input$quanti_subclass_dropdown,
+                              "SCCP" = sccp_matrix(),
+                              "MCCP" = mccp_matrix(),
+                              "LCCP" = lccp_matrix())
+  
+  datatable(matrix_to_display, 
+            selection = list(mode = 'multiple', target = 'cell'),
+            class = 'display cell-border compact nowrap',
+            options = list(info = FALSE, paging = FALSE, dom = 'Bfrtip', 
+                           scrollX = TRUE, scroller = TRUE, bFilter = FALSE, 
+                           ordering = FALSE, 
+                           columnDefs = list(list(className = 'dt-body-justify', 
+                                                  targets = "_all")))
+  )
+})
+
+# Observer pour stocker les sélections en fonction du type
+observeEvent(input$quanti_matrix_homologue_cells_selected, {
+  cells <- input$quanti_matrix_homologue_cells_selected
+  if (!is.null(cells)) {
+    if (input$quanti_subclass_dropdown == "SCCP") {
+      selected_cells_sccp(cells)
+    } else if (input$quanti_subclass_dropdown == "MCCP") {
+      selected_cells_mccp(cells)
+    } else if (input$quanti_subclass_dropdown == "LCCP") {
+      selected_cells_lccp(cells)
+    }
+  }
+})
+
+# Observer pour réappliquer les sélections lors du changement de sous-classe
+observe({
+  cells <- switch(input$quanti_subclass_dropdown,
+                  "SCCP" = selected_cells_sccp(),
+                  "MCCP" = selected_cells_mccp(),
+                  "LCCP" = selected_cells_lccp())
+  
+  if (!is.null(cells)) {
+    proxy <- dataTableProxy('quanti_matrix_homologue')
+    selectCells(proxy, cells)
+  }
+})
+
+output$quanti_matrix_IS <- DT::renderDataTable({
+  # Create an empty matrix with the desired dimensions
+  quanti_matrix_vide <- matrix(ncol = 31, nrow = 28)
+  colnames(quanti_matrix_vide) <- paste0("C", 6:36)
+  rownames(quanti_matrix_vide) <- paste0("Cl", 3:30)
+  
+  # Return the empty matrix as a data frame
+  datatable(as.data.frame(quanti_matrix_vide), 
+            selection = list(mode = 'multiple', target = 'cell'),
+            class = 'display cell-border compact nowrap',
+            options = list(info = FALSE, paging = FALSE, dom = 'Bfrtip', 
+                           scrollX = TRUE, scroller = TRUE, bFilter = FALSE, 
+                           ordering = FALSE, 
+                           columnDefs = list(list(className = 'dt-body-justify', 
+                                                  targets = "_all")))
+  )
 })
--- ui/quantification.R
+++ ui/quantification.R
@@ -15,46 +15,69 @@ shinydashboard::tabItem(
   ),
   shiny::conditionalPanel(
     condition = "input.quantification_choice == 'Sample List'",
-    div(
-      h3("Sample List"),
-      DT::dataTableOutput('quanti_table')
+    shinydashboard::box(
+      width = 12,
+      div(
+        h3("Sample List"),
+        DT::dataTableOutput('quanti_table')
+      )
     )
   ),
   shiny::conditionalPanel(
     condition = "input.quantification_choice == 'Homologue Domain'",
-    div(
-      h3("Homologue Domain")
+    shinydashboard::box(
+      width = 12,
+      shiny::column(width = 12,
+        div(
+          h3("Homologue Domain"),
+          shiny::column(width = 3, 
+            shiny::selectInput('quanti_subclass_dropdown', 'Subclass',
+              choices = c("SCCP", "MCCP", "LCCP")
+            )
+          ),
+          shiny::column(width = 9,
+            DT::dataTableOutput('quanti_matrix_homologue')
+          )
+        )
+      )
     )
   ),
   shiny::conditionalPanel(
     condition = "input.quantification_choice == 'Internal Standard'",
-    div(
-      h3("Internal Standard")
+    shinydashboard::box(
+      width = 12,
+      div(
+        h3("Internal Standard"),
+        DT::dataTableOutput('quanti_matrix_IS')
+      )
     )
   ),
   shiny::conditionalPanel(
     condition = "input.quantification_choice == 'Filters'",
-    div(
-      h3("Filters Form")
-    ),
-    shiny::fluidRow(
-      shiny::column(width = 6, 
-        shiny::numericInput('filter_intensity', 'Minimum Normalized Intensity (xE6)', value = 0, min = 0, max = 100, step = 1)
-      )
-    ),
-    shiny::fluidRow(
-      shiny::column(width = 6, 
-        shiny::numericInput('filter_score', 'Minimum Pattern Score (%)', value = 0, min = 0, max = 100, step = 1)
-      )
-    ),
-    shiny::fluidRow(
-      shiny::column(width = 6, 
-        shiny::numericInput('filter_deviation', 'Minimum Deviation (mDa)', value = 0, step = 0.01)
-      )
-    ),
-    shiny::fluidRow(
-      shiny::column(width = 6,
-        shiny::actionButton("apply_filter", "Apply Filter")
+    shinydashboard::box(
+      width = 12,
+      div(
+        h3("Filters Form")
+      ),
+      shiny::fluidRow(
+        shiny::column(width = 6, 
+          shiny::numericInput('filter_intensity', 'Minimum Normalized Intensity (xE6)', value = 0, min = 0, max = 100, step = 1)
+        )
+      ),
+      shiny::fluidRow(
+        shiny::column(width = 6, 
+          shiny::numericInput('filter_score', 'Minimum Pattern Score (%)', value = 0, min = 0, max = 100, step = 1)
+        )
+      ),
+      shiny::fluidRow(
+        shiny::column(width = 6, 
+          shiny::numericInput('filter_deviation', 'Minimum Deviation (mDa)', value = 0, step = 0.01)
+        )
+      ),
+      shiny::fluidRow(
+        shiny::column(width = 6,
+          shiny::actionButton("apply_filter", "Apply Filter")
+        )
       )
     )
   )
